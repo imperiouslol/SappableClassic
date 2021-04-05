45736b80b9e7fd61b33de99653754ccbe99a9765
Noktrim <53099857+kbabs17@users.noreply.github.com>
2021-04-05 11:56:49 -0400

    Update Sappable.lua
    Added slash commands and resize option

--- Sappable.lua
+++ Sappable.lua
@@ -1,7 +1,5 @@
 targetIndicator=CreateFrame("Frame");
 targetIndicator:SetParent(TargetFrame);
-targetIndicator:SetPoint("Right", TargetFrame, -15);
-targetIndicator:SetSize(26, 26);
 targetIndicator.t=targetIndicator:CreateTexture(nil,BORDER);
 targetIndicator.t:SetAllPoints();
 targetIndicator.t:SetTexture("Interface\\Icons\\ABILITY_SAP");
@@ -9,8 +7,6 @@ targetIndicator:Hide();
 
 focusIndicator=CreateFrame("Frame");
 focusIndicator:SetParent(FocusFrame);
-focusIndicator:SetPoint("Right", FocusFrame, -15);
-focusIndicator:SetSize(26, 26);
 focusIndicator.t=focusIndicator:CreateTexture(nil,BORDER);
 focusIndicator.t:SetAllPoints();
 focusIndicator.t:SetTexture("Interface\\Icons\\ABILITY_SAP");
@@ -28,6 +24,32 @@ tCreatureTypesByLocale["ptBR"]={"Humanoide"};
 tCreatureTypesByLocale["zhCN"]={"人型"};
 tCreatureTypesByLocale["zhTW"]={"人形"};
 
+local function updateIndicatorSize()
+	if indicatorScale == nil then
+		indicatorScale = 26;
+		end
+	targetIndicator.t:ClearAllPoints()
+	targetIndicator.t:SetSize(indicatorScale, indicatorScale)
+	targetIndicator.t:SetPoint("Right", TargetFrame, "Right", indicatorScale/2, 0)
+	focusIndicator.t:ClearAllPoints()
+	focusIndicator.t:SetSize(indicatorScale, indicatorScale)
+	focusIndicator.t:SetPoint("Right", FocusFrame, "Right", indicatorScale/2, 0)
+end;
+
+local function MyAddonCommands(msg)
+	local msg, value = string.split(" ", msg)
+	if msg == 'resize' then
+	indicatorScale = value
+	updateIndicatorSize();
+	print("Sap Indicator resized to ", value)
+	else
+    print("Sappable Options:")
+	print("/sappable resize X")
+  end
+end
+SLASH_SAPPABLE1, SLASH_SAPPABLE2 = '/sappable', '/sap'
+SlashCmdList["SAPPABLE"] = MyAddonCommands
+
 local function creatureIsSappable(unit)
 	creatureType = UnitCreatureType(unit)
 	for index, value in ipairs(tCreatureTypesByLocale[GetLocale()]) do
@@ -38,7 +60,6 @@ local function creatureIsSappable(unit)
 	
 	return false;
 end;
-
 
 local function showIndicator(self, unit)
 	if (creatureIsSappable(unit)
@@ -54,3 +75,6 @@ local t = CreateFrame("Frame")
 t:SetScript("OnUpdate", function(self) showIndicator(targetIndicator, "target") end);
 local f = CreateFrame("Frame")
 f:SetScript("OnUpdate", function(self) showIndicator(focusIndicator, "focus") end);
+local x = CreateFrame("Frame")
+x:RegisterEvent("ADDON_LOADED");
+x:SetScript("OnEvent", updateIndicatorSize);
